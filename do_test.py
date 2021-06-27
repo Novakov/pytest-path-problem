9fd509f5bdaa6d6c1d0c859e0a2c4d9a76dce9fa
Maciej Nowak <maciejt.nowak@gmail.com>
2021-06-27 14:11:02 +0200

    Tool for running hw tests

--- do_test.py
+++ do_test.py
@@ -1,2 +1,16 @@
+import os
+import subprocess
+import sys
+
+
 def do_run():
-    print('Hello World')
+    hw_tests_dir = os.path.join(os.path.dirname(__file__), 'hw_tests')
+    cmdline = [
+        sys.executable,
+        '-m', 'pytest',
+        '--argument', 'abc',
+        hw_tests_dir,
+        *sys.argv[1:]
+    ]
+
+    return subprocess.run(cmdline).returncode
